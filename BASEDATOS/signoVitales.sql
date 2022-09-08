d342e97a2dda7670028ea6d7cc38a1a23a17afda
sanabriacesar86-admin <sanabriacesar86@gmail.com>
2022-09-08 16:20:37 -0500

    Comint2 creacion tabla signosVitales

---
+++ BASEDATOS/signoVitales.sql
@@ -0,0 +1,10 @@
+CREATE TABLE signoVitales(
+	idSignoVitales SERIAl PRIMARY KEY not null,
+	oximetria INTEGER not null,
+	frecuenciaRespiratoria INTEGER not NULL,
+	frecuenciafrecuenciaCardiaca INTEGER not NULL,
+	temperatura INTEGER not NULL,
+	presionArterial INTEGER not NULL,
+	glicemia INTEGER not NULL,
+	fechaHoraSignosVitales DATA not NULL
+);
