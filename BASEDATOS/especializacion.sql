78e1157d9aa2ff4065cb5d19776c7538653b657e
sanabriacesar86-admin <sanabriacesar86@gmail.com>
2022-09-14 22:52:48 -0500

    COMIT8: Se agrego al scritp SQL insercion de datos de los tipos de Especialistas

--- BASEDATOS/especializacion.sql
+++ BASEDATOS/especializacion.sql
@@ -8,3 +8,15 @@ alter table persona
 add constraint FKtest
 Foreign key (idEspecialidad)
 references especializacion(idEspecializacion)
+
+
+insert into especializacion values (1, 'Neurología');
+insert into especializacion values (2, 'Pediatría');
+insert into especializacion values (3, 'Medicina Interna');
+insert into especializacion values (4, 'Anestesia');
+insert into especializacion values (5, 'Ginecología');
+insert into especializacion values (6, 'Urología');
+insert into especializacion values (7, 'Cardiologia');
+insert into especializacion values (8, 'Oftalmología');
+insert into especializacion values (9, 'Ortopedia');
+insert into especializacion values (10, 'Psiquiatría');
