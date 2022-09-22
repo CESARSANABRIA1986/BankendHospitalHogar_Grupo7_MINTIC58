020fa42060a73471133606025b2929253dd80b8e
ceesarsanabria <sanabriacesar@hotmail.com>
2022-09-22 00:55:37 -0500

    configuracion del entorno de proyecto HospitalizacionHogar

--- BACKEND/hospitalizacionHogarCiclo3/settings.py
+++ BACKEND/hospitalizacionHogarCiclo3/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'hospitalizacionHogarCiclo3App'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'hospitalizacionHogarCiclo3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD':'CoYoTe6827/*-+',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -94,6 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
+
+
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
@@ -121,3 +130,28 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMiSSION_CLASSES': {
+        'rest_framework.permission.AllowAny',
+    },
+
+    'DEFAULT_AUTHETICATION_CLASSES':{
+        'rest_framework_simplejwt.autentication.JWTAuthentication',
+    }
+
+}
+
+
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFE': timedelta(days=1),
+    'ROTATE_REFRESH_TOKEN': False,
+    'BLACKLIST_AFTER_ROTATION':True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM':  'user_id',
+} 
---
+++ BACKEND/hospitalizacionHogarCiclo3App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Hospitalizacionhogarciclo3AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hospitalizacionHogarCiclo3App'
