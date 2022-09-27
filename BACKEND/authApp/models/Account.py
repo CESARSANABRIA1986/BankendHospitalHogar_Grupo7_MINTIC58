428971c3c014dad122c8b3a9e07a95e2a7134401
ceesarsanabria <sanabriacesar@hotmail.com>
2022-09-26 20:10:30 -0500

    Modificacion models Account se agrego el item fecha Ingreso del Medico

--- BACKEND/authApp/models/Account.py
+++ BACKEND/authApp/models/Account.py
@@ -4,8 +4,7 @@ from .User import User
 
 
 class Account(models.Model):
-    idAcount = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, related_name='account', on_delete=models.CASCADE)
-    balance = models.IntegerField(default=0)
-    lastChangeDate = models.DateTimeField()
-    isActive = models.BooleanField(default=True)
+    especializacion = models.CharField('Especializacion', max_length=100)
+    fechaIngreso = models.DateTimeField()
